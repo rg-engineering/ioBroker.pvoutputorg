b9410bcca0d0a5a49ba158e96a387d7d0b8dd244
Seqway <dirkscheck@gmx.de>
2020-10-19 07:07:17 +0000

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"pvoutputorg adapter settings": {
+		"en": "Adapter settings for pvoutputorg",
+		"de": "Adaptereinstellungen für pvoutputorg",
+		"ru": "Настройки адаптера для pvoutputorg",
+		"pt": "Configurações do adaptador para pvoutputorg",
+		"nl": "Adapterinstellingen voor pvoutputorg",
+		"fr": "Paramètres d'adaptateur pour pvoutputorg",
+		"it": "Impostazioni dell'adattatore per pvoutputorg",
+		"es": "Ajustes del adaptador para pvoutputorg",
+		"pl": "Ustawienia adaptera dla pvoutputorg",
+		"zh-cn": "pvoutputorg的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "option1",
+		"ru": "option1",
+		"pt": "option1",
+		"nl": "option1",
+		"fr": "option1",
+		"it": "option1",
+		"es": "option1",
+		"pl": "option1",
+		"zh-cn": "option1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "option2",
+		"ru": "option2",
+		"pt": "option2",
+		"nl": "option2",
+		"fr": "option2",
+		"it": "option2",
+		"es": "option2",
+		"pl": "option2",
+		"zh-cn": "option2"
+	}
+};
